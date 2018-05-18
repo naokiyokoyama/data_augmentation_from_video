17dd4ed612147d1dd0858124f73051585e5b089d
naokiyokoyama <naoki.yokoyama7@gmail.com>
2018-05-18 00:07:11 -0400

    added run all routine

--- generate_composites_from_videos.py
+++ generate_composites_from_videos.py
@@ -6,8 +6,6 @@ import numpy as np
 import random as rand
 import sys
 
-COMPOSITES = int(sys.argv[1])
-
 HEADER = 'filename,width,height,class,xmin,ymin,xmax,ymax\n'
 
 def randomFrame(class_id):
@@ -28,60 +26,64 @@ def write2CSV(csv,params):
 	newline = ",".join(str(x) for x in params)
 	csv.write(newline+'\n')
 
+def main(COMPOSITES,NAME):
+	train_csv = open(NAME+'_train.csv','w')
+	train_csv.write(HEADER)
+	# How many different classes?
+	num_class = 0
+	for vid_path in glob.glob('data/videos/*.MOV'):
+		if len(vid_path.split('-')) == 2:
+			num_class += 1
+	print("Detected %s different classes" % num_class)
+	# Array of background images
+	bg_list = []
+	for img_path in glob.glob('data/bg/*.JPG'):
+		img = cv2.imread(img_path)
+		bg_list.append(img)
+	# Create composites
+	directory = 'data/generated_pictures/images_'+NAME
+	if not os.path.exists(directory):
+		os.makedirs(directory)
+	# for each composite,
+	for x in range(0,COMPOSITES):
+		# select background
+		bg_index = np.random.randint(len(bg_list))
+		bg = bg_list[bg_index]
+		# create empty background
+		layers = cv2.cvtColor(bg.copy(),cv2.COLOR_BGR2BGRA)
+		layers[:,:,:] = 0
+		fac = 1
+		chosen_classes_amount = 4
+		resize_min = 0.2
+		resize_max = 0.6
+		resize_span = (resize_max-resize_min)/chosen_classes_amount
+		chosen_classes = rand.sample(range(num_class),chosen_classes_amount)
+		# for each class,
+		filename = directory+'/'+str(x)+'.JPG'
+		for class_id in chosen_classes:
+			img,class_name = randomFrame(class_id)
+			# img = distorter.resizeRandom(img,0.2,0.5,bg.shape)
+			img = distorter.resizeRandom(img,resize_max-resize_span*fac,resize_max-resize_span*(fac-1),bg.shape)
+			fac += 1
+			width,height,xmin,ymin,xmax,ymax = masker.createComposite(img,layers)
+			composite = cv2.cvtColor(bg,cv2.COLOR_BGR2BGRA)
+			label = class_name+'-'+str(class_id)
+			write2CSV(train_csv,[filename,width,height,label,xmin,ymin,xmax,ymax])
+		composite = cv2.cvtColor(bg.copy(),cv2.COLOR_BGR2BGRA)
+		composite[np.where((layers!=[0,0,0,0]).all(axis=2))] = layers[np.where((layers!=[0,0,0,0]).all(axis=2))]
+		composite[:,:,3] = 255 # Empty
+		composite = cv2.cvtColor(composite,cv2.COLOR_BGRA2BGR)
+		composite = distorter.randomGamma(composite,3.5)
+		composite = distorter.randomBlur(composite,2)
+		# composite = distorter.randomNoise(composite)
+		cv2.imwrite(filename,composite)
 
-train_csv = open('train.csv','w')
-train_csv.write(HEADER)
-# How many different classes?
-num_class = 0
-for vid_path in glob.glob('data/videos/*.MOV'):
-	if len(vid_path.split('-')) == 2:
-		num_class += 1
-print("Detected %s different classes" % num_class)
-# Array of background images
-bg_list = []
-for img_path in glob.glob('data/bg/*.JPG'):
-	img = cv2.imread(img_path)
-	bg_list.append(img)
-# Create composites
-directory = 'data/generated_images/composites'
-if not os.path.exists(directory):
-	os.makedirs(directory)
-# for each composite,
-for x in range(0,COMPOSITES):
-	# select background
-	bg_index = np.random.randint(len(bg_list))
-	bg = bg_list[bg_index]
-	# create empty background
-	layers = cv2.cvtColor(bg.copy(),cv2.COLOR_BGR2BGRA)
-	layers[:,:,:] = 0
-	# 0-2 instances per class, but at least one
-	# num_inst_each_class = 0
-	# while np.sum(num_inst_each_class) == 0:
-	# 	num_inst_each_class = [np.random.randint(3) for xx in range(num_class)]
-	chosen_classes = rand.sample(range(num_class),5)
-	# for each class,
-	filename = directory+'/'+str(x)+'.JPG'
-	for class_id in chosen_classes:
-		img,class_name = randomFrame(class_id)
-		img = distorter.resizeRandom(img,0.2,0.4,bg.shape)
-		width,height,xmin,ymin,xmax,ymax = masker.createComposite(img,layers)
-		composite = cv2.cvtColor(bg,cv2.COLOR_BGR2BGRA)
-		label = class_name+'-'+str(class_id)
-		write2CSV(train_csv,[filename,width,height,label,xmin,ymin,xmax,ymax])
-	# for class_id in range(num_class):
-	# 	# for each instance
-	# 	for xxx in range(num_inst_each_class[class_id]):
-	# 		img,class_name = randomFrame(class_id)
-	# 		img = distorter.resizeRandom(img,0.2,0.6,bg.shape)
-	# 		width,height,xmin,ymin,xmax,ymax = masker.createComposite(img,layers)
-	# 		composite = cv2.cvtColor(bg,cv2.COLOR_BGR2BGRA)
-	# 		label = class_name+'-'+str(class_id)
-	# 		write2CSV(train_csv,[filename,width,height,label,xmin,ymin,xmax,ymax])
-	composite = cv2.cvtColor(bg.copy(),cv2.COLOR_BGR2BGRA)
-	composite[np.where((layers!=[0,0,0,0]).all(axis=2))] = layers[np.where((layers!=[0,0,0,0]).all(axis=2))]
-	composite[:,:,3] = 255 # Empty
-	composite = cv2.cvtColor(composite,cv2.COLOR_BGRA2BGR)
-	composite = distorter.randomGamma(composite,1.8)
-	composite = distorter.randomBlur(composite,2)
-	# composite = distorter.randomNoise(composite)
-	cv2.imwrite(filename,composite)
+if __name__ == "__main__":
+	if len(sys.argv) < 2:
+		print "Not enough args given!"
+		print "Arg1: number of images to generate"
+		print "Arg2: desired name of training set"
+		sys.exit()
+	COMPOSITES = int(sys.argv[1])
+	NAME = sys.argv[2]
+	main(COMPOSITES,NAME)
