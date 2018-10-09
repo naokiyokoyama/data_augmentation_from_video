added rotation and attempt at occlusion fix

generate_composites_from_videos.py
@@ -7,6 +7,7 @@ import random as rand
 import sys
 
 HEADER = 'filename,width,height,class,xmin,ymin,xmax,ymax\n'
+NUM_CLASSES_IN_EACH_COMPOSITE = 4
 
 def random_extract(class_id):
 	possible_paths = glob.glob('data/videos/'+str(class_id)+'-*')
@@ -14,9 +15,8 @@ def random_extract(class_id):
 	png_dir = png_dirs[np.random.randint(len(png_dirs))]
 	img_path = rand.choice(glob.glob(png_dir+'/*'))
 	img = cv2.imread(img_path,cv2.IMREAD_UNCHANGED)
-	# print img
-	# cv2.imshow('',img);cv2.waitKey();cv2.destroyAllWindows()
 	class_name = png_dir.split('-')[-1].split('.')[0]
+	img = masker.rotateObject(img)
 	return img,class_name
 
 def write2CSV(csv,params):
@@ -28,59 +28,54 @@ def main(START_INDEX,END_INDEX,NAME):
 	train_csv.write(HEADER)
 	# How many different classes?
 	num_class = max([int(vid_path.split('/')[-1].split('-')[0]) for vid_path in glob.glob('data/videos/*')])+1
-
 	print("Detected %s different classes" % num_class)
+
 	# Array of background images
-	bg_list = []
-	for img_path in glob.glob('data/bg/*.JPG'):
-		img = cv2.imread(img_path)
-		bg_list.append(img)
+	bg_list = [cv2.imread(i) for i in glob.glob('data/bg/*.JPG')]
+	
 	# Create RCNN annotations
 	directory = 'data/generated_pictures/annotations_'+NAME
 	if not os.path.exists(directory):
 		os.makedirs(directory)
+	
 	# Create composites
 	directory = 'data/generated_pictures/images_'+NAME
 	if not os.path.exists(directory):
 		os.makedirs(directory)
 		
-	# for each composite,
+	# For each composite,
 	for x in range(START_INDEX,END_INDEX+1):
-		# select background
-		bg_index = np.random.randint(len(bg_list))
-		bg = bg_list[bg_index]
-		# create empty background
-		layers = cv2.cvtColor(bg,cv2.COLOR_BGR2BGRA)
-		layers[:,:,:] = 0
-		chosen_classes_amount = 4
-		chosen_classes = rand.sample(range(num_class),chosen_classes_amount)
-		# for each class,
-		filename = directory+'/'+str(x)+'.JPG'
-		classes_list = list()
+		# Randomly select background
+		bg = rand.choice(bg_list)
+		
+		# Create empty background
+		layers = np.zeros((bg.shape[0],bg.shape[1],4), np.uint8)
 		rcnn_mask = layers.copy()
+		
+		# For each class,
+		chosen_classes = rand.sample(range(num_class),NUM_CLASSES_IN_EACH_COMPOSITE)
+		classes_list = list()
+
+		filename = directory+'/'+str(x)+'.JPG'
 		for class_id in chosen_classes:
 			ret = False
 			while not ret:
 				img,class_name = random_extract(class_id)
 				img = distorter.resize_by_dim_and_area(img,bg)
 				ret,width,height,xmin,ymin,xmax,ymax = masker.createComposite(img,layers,rcnn_mask,class_name,classes_list)
-				# ret,width,height,xmin,ymin,xmax,ymax = masker.createComposite(img,layers)
-			label = class_name+'-'+str(class_id)
-			write2CSV(train_csv,[filename,width,height,label,xmin,ymin,xmax,ymax])
-		composite = cv2.cvtColor(bg,cv2.COLOR_BGR2BGRA)
+				# ret,width,height,xmin,ymin,xmax,ymax = masker.createComposite(img,layers) # FOR DARKNET
+			# label = class_name+'-'+str(class_id) # FOR DARKNET
+			# write2CSV(train_csv,[filename,width,height,label,xmin,ymin,xmax,ymax]) # FOR DARKNET
 		for i in xrange(layers.shape[0]):
 			for j in xrange(layers.shape[1]):
 				if layers[i,j,3] > 0:
-					composite[i,j,:] = layers[i,j,:]
-		# composite[np.where((layers!=[0,0,0,0]).all(axis=2))] = layers[np.where((layers!=[0,0,0,0]).all(axis=2))]
-		# composite[:,:,3] = 255 # Empty
-		composite = cv2.cvtColor(composite,cv2.COLOR_BGRA2BGR)
+					composite[i,j,:] = layers[i,j,:3]
 		composite = distorter.randomGamma(composite,3.5)
 		composite = distorter.randomBlur(composite,2)
 		# composite = distorter.randomNoise(composite)
 		cv2.imwrite(filename,composite)
 		masker.generate_rcnn_masks(filename,rcnn_mask,classes_list)
-		sys.stdout.write('\r'+str(x)+' of '+str(END_INDEX-START_INDEX+1)+' generated')
+		sys.stdout.write('\r'+str(x-START_INDEX)+' of '+str(END_INDEX-START_INDEX+1)+' generated')
 		sys.stdout.flush()
 
 if __name__ == "__main__":

masker.py
@@ -2,14 +2,6 @@ import cv2
 import numpy as np
 import time
 
-def bgsegmMask(bg,img):
-	fgbg = cv2.bgsegm.createBackgroundSubtractorMOG()
-	fgmask = fgbg.apply(bg)
-	# fgmask = fgbg.apply(bg)
-	# fgmask = fgbg.apply(bg)
-	fgmask = fgbg.apply(img)
-	return fgmask
-
 def cropBox(img): # Input: Image with only the object
 	mask = img[:,:,3]
 	x,y,w,h = cv2.boundingRect(mask)
@@ -68,33 +60,45 @@ def extractObject(bg,img):
 	img[mask==0] = (0,0,0,0)
 	img = cropBox(img)
 	return img
-def extractObjectold(bg,img):
-	img = cv2.resize(img,(960,540))
-	mask = bgsegmMask(bg,img)
-	mask = inflateErode(mask,40)
-	mask = erodeInflateSmart(mask,20)
-	mask = contourMask(mask)
-	img = cv2.cvtColor(img,cv2.COLOR_BGR2BGRA)
-	img[mask==0] = (0,0,0,0)
-	img = cropBox(img)
-	return img
+
+def rotateObject(img):
+	height = img.shape[0]
+	width = img.shape[1]
+	hypotenuse = int(np.ceil(sqrt(height**2+width**2)))
+	blank_image = np.zeros((hypotenuse,hypotenuse,4), np.uint8)
+	x_offset = (hypotenuse-width)/2
+	y_offset = (hypotenuse-height)/2
+	blank_image[y_offset:y_offset+height,x_offset:x_offset+width]=img
+	rotation_degrees = np.random.randint(90)*4
+	M = cv2.getRotationMatrix2D((hypotenuse/2,hypotenuse/2),rotation_degrees,1)
+	dst = cv2.warpAffine(blank_image,M,(hypotenuse,hypotenuse))
+	dst = cropBox(dst)
+	return dst
 
 def createComposite(img,mask,rcnn_mask=None,class_name=None,classes_list=None):
 	timeup = time.time()+10
 	while 1:
-		single_layer = mask.copy()
-		single_layer[:,:,:] = 0
+		single_layer = np.zeros(mask.shape, np.uint8)
 		ymin = np.random.randint(single_layer.shape[0]-img.shape[0]+1)
-		ymax = ymin+img.shape[0]
 		xmin = np.random.randint(single_layer.shape[1]-img.shape[1]+1)
+		ymax = ymin+img.shape[0]
 		xmax = xmin+img.shape[1]
-		single_layer[ymin:ymax,xmin:xmax] = img
+		
+		for i in xrange(ymin,ymax):
+			for j in xrange(xmin,xmax):
+				if img[i,j,3] != 0:
+					single_layer[i,j] = img[i,j]
+
 		# Use the single layer as a mask against the final layer
-		# if the result has any non pure green pixels, try again
+		# if the result has too many unmasked pixels, try again
 		mask_test = mask.copy()
-		mask_test[np.where((single_layer==[0,0,0,0]).all(axis=2))] = [0,0,0,0]
-		original_pixels = 0
-		occluded_pixels = 0
+		for i in xrange(mask.shape[0]):
+			for j in xrange(mask.shape[1]):
+				if sum(single_layer[i,j]) == 0:
+					mask_test[i,j] = single_layer[i,j]
+
+		original_pixels = 0.0
+		occluded_pixels = 0.0
 		for i in xrange(mask.shape[0]):
 			for j in xrange(mask.shape[1]):
 				if single_layer[i,j,3] != 0:
@@ -104,7 +108,10 @@ def createComposite(img,mask,rcnn_mask=None,class_name=None,classes_list=None):
 		occlusion_percentage = occluded_pixels/original_pixels
 
 		if occlusion_percentage<0.5:
-			mask[ymin:ymax,xmin:xmax] = img
+			for i in xrange(ymin,ymax):
+				for j in xrange(xmin,xmax):
+					if img[i,j,3] != 0:
+						mask[i,j] = img[i,j]
 			if rcnn_mask is not None:
 				mask_pixel_value = max(np.ndarray.flatten(rcnn_mask))+1
 				rcnn_mask[np.where((single_layer!=[0,0,0,0]).all(axis=2))] = [0,0,0,mask_pixel_value]
@@ -134,14 +141,3 @@ def generate_rcnn_masks(image_path,rcnn_mask,classes_list):
 		mask_path = '-'.join(mask_path)
 		mask_path = mask_path.replace('images_','annotations_')
 		cv2.imwrite(mask_path,mask)
-
-
-
-
-
-
-
-
-
-
-